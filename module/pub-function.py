3440ad893df897e1b51c99cf32328b6689c6585c
Zreaei <zenarthxdgt@gmail.com>
2023-12-19 11:03:29 +0700

    Changing the logic

---
+++ module/pub-function.py
@@ -0,0 +1,7 @@
+# def detail_gadget():
+#     for gadget in gadgets:
+#         for key, value in gadget.items():
+#             print(f"{key}: {value}")
+#         print("\n")
+        
+# detail_gadget()
